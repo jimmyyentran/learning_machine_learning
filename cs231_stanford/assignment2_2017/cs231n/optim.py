4a45f3bcd1e6c8a0f79012258aa3171547ab74fe
Jimmy Tran <jimmyytran@gmail.com>
2017-05-22 22:34:24 -0700

    finish rmsprop

--- cs231_stanford/assignment2_2017/cs231n/optim.py
+++ cs231_stanford/assignment2_2017/cs231n/optim.py
@@ -108,7 +108,11 @@ def rmsprop(x, dx, config=None):
     # in the next_x variable. Don't forget to update cache value stored in    #
     # config['cache'].                                                        #
     ###########################################################################
-    pass
+    # cache is slightly taken away by the decay_rate and is replaced by dx**2
+    # therefore is considered to be 'leaky'
+    # cache no longer decrease monotonically, but take slight variations in dx
+    config['cache'] = config['decay_rate'] * config['cache'] + (1 - config['decay_rate']) * dx**2
+    next_x = x - config['learning_rate'] * dx / (np.sqrt(config['cache']) + config['epsilon'])
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
